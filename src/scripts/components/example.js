45a570ea73f022df7b90f18a21bf2510e6cd9d08
Alexander Nanberg <alexander.nanberg@strateg.se>
2017-06-15 16:03:26 +0200

    FIx example code

--- src/scripts/components/example.js
+++ src/scripts/components/example.js
@@ -1,8 +1,8 @@
-import { viewport, events, Cookie } from '../utils';
+import { Viewport, Events, Cookie } from '../utils';
 
 function example() {
   // Get viewport example
-  console.log('🖥', viewport.breakpoint);
+  console.log('🖥', Viewport.breakpoint);
 
   // Set cookie example
   Cookie.set('cookie', 'This is a sample cookie', 1);
@@ -11,7 +11,7 @@ function example() {
   console.log('🍪', Cookie.get('cookie'));
 
   // Events subscription example
-  events.on('breakpoint', (bp) => {
+  Events.on('breakpoint', (bp) => {
     console.log('🖥', bp);
   });
 }
