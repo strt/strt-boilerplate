1da97b53be5cfee966f536d527c525b7632bbf79
Alexander Nanberg <alexander.nanberg@strateg.se>
2017-07-17 08:52:06 +0200

    Update title and description

--- src/scripts/utils/Events.js
+++ src/scripts/utils/Events.js
@@ -1,16 +1,34 @@
+/**
+ * @class Events
+ */
 class Events {
   events = [];
 
+  /**
+   *
+   * @param {String} name
+   * @param {Void} fn
+   */
   on(name, fn) {
     this.events[name] = this.events[name] || [];
     this.events[name].push(fn);
   }
 
+  /**
+   *
+   * @param {String} name
+   * @param {Void} fn
+   */
   off(name, fn) {
     if (!this.events[name]) return;
     this.events[name] = this.events[name].filter(item => item !== fn);
   }
 
+  /**
+   *
+   * @param {String} name
+   * @param {Any} data
+   */
   emit(name, data) {
     if (!this.events[name]) return;
     this.events[name].forEach(fn => fn(data));
--- src/scripts/utils/index.js
+++ src/scripts/utils/index.js
@@ -1,5 +1,11 @@
 import Cookie from './Cookie';
 import Viewport from './Viewport';
 import Events from './Events';
+import debounce from './debounce';
 
-export { Cookie, Viewport, Events };
+export {
+  Cookie,
+  Viewport,
+  Events,
+  debounce,
+};
