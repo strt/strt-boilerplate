faf4f91ac8ecc7a45d3ab8bde8be4e8d4fe2e963
Alexander Nanberg <alexander.nanberg@strateg.se>
2017-09-12 11:49:22 +0200

    Remove debounce util (just use lodash instead if needed)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
+// add needed core-js polyfills here
 import 'js-polyfills/dom';
 import 'js-polyfills/html';
 import 'svgxuse';
--- src/scripts/utils/index.js
+++ src/scripts/utils/index.js
@@ -1,11 +1,9 @@
 import Cookie from './Cookie';
 import Viewport from './Viewport';
 import Events from './Events';
-import debounce from './debounce';
 
 export {
   Cookie,
   Viewport,
   Events,
-  debounce,
 };
