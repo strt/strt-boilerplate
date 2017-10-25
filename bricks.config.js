dbfff3dfd90aa0a18d1e07dd84066feae048c34a
Alexander Nanberg <alex.nanberg@hotmail.com>
2017-10-25 11:19:09 +0200

    Rename main to app

--- bricks.config.js
+++ bricks.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   output: 'public/dist',
+  styles: {
+    entries: ['./app.scss'],
+  },
   scripts: {
     publicPath: '/dist/scripts',
+    entries: {
+      app: './app.js',
+    },
   },
   serve: {
     server: 'public',
